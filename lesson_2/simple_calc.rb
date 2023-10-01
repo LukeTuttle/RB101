6666eb8ba5a5ac183b7e3793039ea951a094722c
Luke Tuttle <luke.c.tuttle@gmail.com>
2023-10-01 13:30:03 -0600

    added spanish msgs for calculator

--- lesson_2/simple_calc.rb
+++ lesson_2/simple_calc.rb
@@ -1,5 +1,18 @@
-def prompt(message)
-  puts "=> #{message}"
+require 'yaml'
+MESSAGES = YAML.load_file('calculator_messages.yml')
+LANGUAGE = 'en'
+
+def messages(key, lang='en')
+  MESSAGES[lang][key]
+end
+
+def prompt(message, override=false)
+  if override
+    puts "=> #{message}"
+  else
+    message_output = messages(message, LANGUAGE)
+    puts "=> #{message_output}"
+  end
 end
 
 def valid_number?(num)
@@ -20,72 +33,72 @@ def do_math(left, right, operator)
 end
 
 def operation_to_message(op)
-  case op
-  when '+'
-    'Adding'
-  when '-'
-    'Subtracting'
-  when 'x'
-    'Multiplying'
-  when '/'
-    'Dividing'
-  end
+  msg = case op
+        when '+'
+          'Adding'
+        when '-'
+          'Subtracting'
+        when 'x'
+          'Multiplying'
+        when '/'
+          'Dividing'
+        end
+  msg
 end
 
-prompt "Welcome to the Simple Calculator! Enter your name:"
+prompt('welcome')
 
 name = ''
 loop do
   name = gets.chomp
   if name.empty?
-    prompt "Make sure to use a valid name."
+    prompt('valid_name')
   else
     break
   end
 end
 
-prompt "Hi #{name}"
+prompt("Hi #{name}", true)
 # main loop
 loop do
-  prompt "enter the number on the left hand side"
+  prompt("first_num")
   left_operand = ''
   loop do
     left_operand = gets.chomp.to_i
     if valid_number?(left_operand)
       break
     else
-      prompt("Hmm... that doesn't look like a valid number")
+      prompt("valid_number")
     end
   end
 
-  prompt "enter the number on the right hand side"
+  prompt("second_num")
   right_operand = ''
   loop do
     right_operand = gets.chomp.to_i
     if valid_number?(right_operand)
       break
     else
-      prompt("Hmm... that doesn't look like a valid number")
+      prompt("valid_number")
     end
   end
 
-  prompt "Now enter the appropriate operation to be performed (+ - x /)"
-
+  prompt('operation')
   operator = ''
   loop do
     operator = gets.chomp
     break if %w(x - + /).include? operator
-    prompt 'You must choose (x - + /)'
+    prompt('valid_operation')
   end
 
-  prompt("#{operation_to_message(operator)} the two numbers...")
+  prompt("#{operation_to_message(operator)} the two numbers...", true)
 
   result = do_math(left_operand, right_operand, operator)
-  prompt "#{left_operand} #{operator} #{right_operand} = #{result}"
+  prompt("#{left_operand} #{operator} #{right_operand} = #{result}", true)
 
-  prompt "Do you want to perform another calculation? (Y to calculate again)"
+  prompt('go_again?')
   answer = gets.chomp
   break unless answer.downcase.start_with? 'y'
 end
 
-prompt "Thank you for using the calculator"
+prompt('goodbye')
